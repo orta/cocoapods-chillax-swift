51f071869de44a39d73488966c8d330036c9153c
Orta Therox <orta.therox@gmail.com>
2014-12-31 12:03:51 +0000

    rough

--- lib/plugin.rb
+++ lib/plugin.rb
@@ -1,7 +1,1 @@
-module CocoaPodsChillaxSwift
-  Pod::HooksManager.register('cocoapods-chillax-swift', :post_install) do |context, user_options|
-    require 'installer'
-
-    Installer.new(context.sandbox_root, user_options).install!
-  end
-end
+puts "ERR?"
